10e9c22d257b7b9ce3477171d65a1bf9dcedb9cf
Frej Alexander Nielsen <frejnielsen59@gmail.com>
2016-05-28 14:09:45 +0200

    Added game loop.

--- Game/src/com/CountedReturns/Game/Main.java
+++ Game/src/com/CountedReturns/Game/Main.java
@@ -1,9 +1,67 @@
 package com.CountedReturns.Game;
 
-public class Main {
+import java.awt.Canvas;
+
+public class Main extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	
+	//VARIABLES BELOW
+	private boolean running = false;
+	
+	private int WIDTH = 640;
+	private int HEIGHT = 480;
+	
+	private int fps = 0;
+	private int ups = 0;
+	
+	//END OF VARIABLES
+		
 
 	public static void main(String[] args) {
-
+		
 	}
+	
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 20.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
 
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				fps = frames;
+				ups = updates;
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	private void render() {
+		
+	}
+	
+	private void tick() {
+		System.out.println("FPS:" + fps);
+		System.out.println("Ticks: " + ups);
+		System.out.println("Width: " + WIDTH);
+		System.out.println("Height: " + HEIGHT);
+	}
+	
 }
